0915f7045a25e0f3d49a95ce5d7f7bd2583c516d
Quasim Rizvi <sr5742@nyu.edu>
2025-02-16 13:48:05 -0600

    Commit: Added application

---
+++ junit-tutorial/src/main/java/com/quasiriz/junittutorial/JunitTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.quasiriz.junittutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTutorialApplication.class, args);
+	}
+
+}
